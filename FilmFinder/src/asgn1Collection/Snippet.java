d9840050f1ac231261e7528b74baebcddb62c1fb
James Flannery <jwflannery93@gmail.com>
2015-03-25 10:08:55 +1000

    Method Stubs

--- FilmFinder/src/asgn1Collection/Snippet.java
+++ FilmFinder/src/asgn1Collection/Snippet.java
@@ -1,11 +1,11 @@
-package asgn1Collection;
+/*package asgn1Collection;
 
 //THis is some free code for you to include (adapting as needed) for inclusion in the MovieListing.java 
 //class that you have to create. It is copied from that class before it was deleted. 
 public class Snippet {
 	/* (non-Javadoc)
 		 * @see java.lang.Object#toString()
-		 */
+		 *
 		@Override
 		public String toString() {
 			return this.title + ":" + this.year + ":" + "Active keywords:" + this.numKeywords();
@@ -14,7 +14,7 @@ public class Snippet {
 	
 		/* (non-Javadoc)
 		 * @see asgn1Collection.Listing#writeKeywords()
-		 */
+		 *
 		public String writeKeywords() {
 			String str=""; int index=0;
 			for (String kw : this.keywords) {
@@ -27,5 +27,5 @@ public class Snippet {
 			return str;
 		}
 		
-}
+}*/
 
